3c55a691272eac441b8221b69d603890c17e94e7
MattFiler <matthew.filer@hotmail.co.uk>
2020-02-09 12:27:58 +0000

    Tidying MATLAB plotting

--- MATLAB/LoadFromID.m
+++ MATLAB/LoadFromID.m
@@ -1,4 +1,4 @@
-function [hdrlum, ldrlum ldrchroma] = LoadFromID(ID)
+function [hdrlum, ldrlum] = LoadFromID(ID)
     image = hdrread(strcat('../Output/Images/',strcat(ID,'.hdr'))); 
     ycbcr = rgb2ycbcr(image); 
     hdrlum = ycbcr(:,:,1);
@@ -6,6 +6,5 @@ function [hdrlum, ldrlum ldrchroma] = LoadFromID(ID)
     image = imread(strcat('../Output/Images/',strcat(ID,'_shifted.jpg')));
     ycbcr = rgb2ycbcr(image);
     ldrlum = single(ycbcr(:,:,1)) ./ 255.0;
-    ldrchroma = single(ycbcr(:, :, 2 : 3)) ./ 255.0;
 end
 
--- MATLAB/lumachroma.m
+++ MATLAB/lumachroma.m
@@ -1,41 +1,18 @@
 STREETVIEW_ID = 'xdU_R-qfflPfs8x-tTKM8g';
 
-[hdrlum, ldrlum ldrchroma] = LoadFromID(STREETVIEW_ID);
+[hdrlum, ldrlum] = LoadFromID(STREETVIEW_ID);
 
+% Create a histogram from the luma data
 hdrhist = hist(reshape(hdrlum, [64 * 128, 1]), 100);
 ldrhist = hist(reshape(ldrlum, [size(ldrlum, 1) * size(ldrlum, 2), 1]), 100);
 
+% Bring our values down to a normalised range
 hdrhist = hdrhist ./ (64 * 128);
 ldrhist = ldrhist ./ (size(ldrlum, 1) * size(ldrlum, 2));
-% 
-% %Downscaled LDR
-% image = imread(strcat('../Output/Images/',strcat(STREETVIEW_ID,'_downscaled.jpg')));
-% ycbcr = rgb2ycbcr(image);
-% y = ycbcr(:,:,1);
-% figure
-% imshow(y);
-% title('Downscaled LDR Image Luma');
-% 
-% %Regular size LDR
-% image = imread(strcat('../Output/Images/',strcat(STREETVIEW_ID,'_shifted.jpg')));
-% ycbcr = rgb2ycbcr(image);
-% y = ycbcr(:,:,1);
-% figure
-% imshow(y);
-% title('Regular LDR Image Luma');
-% 
-% %Regular size HDR
-% image = hdrread(strcat('../Output/Images/',strcat(STREETVIEW_ID,'.hdr'))); 
-% ycbcr = rgb2ycbcr(image); 
-% y = ycbcr(:,:,1);
-% figure
-% imshow(y); 
-% title('Regular HDR Image Luma');
-% 
-% %Upscaled HDR
-% image = hdrread(strcat('../Output/Images/',strcat(STREETVIEW_ID,'_upscaled.hdr'))); 
-% ycbcr = rgb2ycbcr(image); 
-% y = ycbcr(:,:,1);
-% figure
-% imshow(y); 
-% title('Upscaled HDR Image Luma');
+
+% Plot out the values
+clf;
+plot(ldrhist, 'DisplayName', 'LDR Luma');
+hold on;
+plot(hdrhist, 'DisplayName', 'HDR Luma');
+legend;
