aab04f375d42d027fbb8d554755c890653bdb22c
MattFiler <matthew.filer@hotmail.co.uk>
2020-02-09 12:55:24 +0000

    Try and match plots

--- MATLAB/lumachroma.m
+++ MATLAB/lumachroma.m
@@ -1,5 +1,5 @@
+% Load luma data from LDR/HDR images matching ID
 STREETVIEW_ID = 'xdU_R-qfflPfs8x-tTKM8g';
-
 [hdrlum, ldrlum] = LoadFromID(STREETVIEW_ID);
 
 % Create a histogram from the luma data
@@ -15,4 +15,30 @@ clf;
 plot(ldrhist, 'DisplayName', 'LDR Luma');
 hold on;
 plot(hdrhist, 'DisplayName', 'HDR Luma');
+%legend;
+
+% Try and match both distributions
+hdrhistmod = zeros(1, 100);
+hdrhistdiff = zeros(1, 100);
+leftover = 0;
+for x = 1:100
+    thisLDRValue = ldrhist(1, x);
+    thisHDRValue = hdrhist(1, x);
+    
+    leftover = leftover + thisHDRValue;
+    hdrhistmod(1, x) = thisLDRValue;
+    leftover = leftover - thisLDRValue;
+    hdrhistdiff(1, x) = leftover;
+    
+    if leftover <= 0
+        %error("Out of entries at iteration " + c);
+        break;
+    end
+end
+
+% Re-plot
+hold on;
+plot(hdrhistmod, 'DisplayName', 'HDR Luma Adjusted');
+hold on;
+plot(hdrhistdiff, 'DisplayName', 'HDR Luma Diff');
 legend;
