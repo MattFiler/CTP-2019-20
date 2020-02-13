639b7b49d8f298a49048a4447ea388735d26caa9
MattFiler <matthew.filer@hotmail.co.uk>
2020-02-13 20:15:27 +0000

    Added neural-doodle

---
+++ neural-doodle/run.bat
@@ -0,0 +1,3 @@
+call conda activate tensorflow_cpu
+python doodle.py --style samples/xdU_R-qfflPfs8x-tTKM8g_sml.jpg --content samples/_e6sfDnDHZ8AI11ocRSZJA_sml.png --output new__blop.png --device=cpu --phases=4 --iterations=40
+pause
